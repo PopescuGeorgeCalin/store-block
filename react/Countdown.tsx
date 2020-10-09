76c2a6c2db6ddbb2d192ef6d322b012126367e79
Calin Popescu <calin.popescu@vtex.com.br>
2020-10-09 09:07:55 +0300

    checkpoint

--- react/Countdown.tsx
+++ react/Countdown.tsx
@@ -1,16 +1,76 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { TimeSplit } from './typings/global';
+import { tick } from './utils/time';
+import { useCssHandles } from 'vtex.css-handles';
+import { useQuery } from 'react-apollo';
+import useProduct from 'vtex.product-context/useProduct';
+import productReleaseDate from './queries/productReleaseDate.graphql';
 
-interface CountdownProps { }
+const CSS_HANDLES = ["countdown"];
+
+interface CountdownProps {
+  targetDate: string
+}
 
 const Countdown: StorefrontFunctionComponent<CountdownProps> = ({ }) => {
-  return <div></div>
+  const { product } = useProduct();
+  const { data, loading, error } = useQuery(productReleaseDate, {
+    variables: {
+      slug: product?.linkText
+    },
+    ssr: false
+  });
+  const [timeRemaining, setTime] = useState<TimeSplit>({
+    hours: '00',
+    minutes: '00',
+    seconds: '00'
+  });
+
+  const handles = useCssHandles(CSS_HANDLES);
+  
+  tick(data?.product?.releaseDate, setTime);
+
+  if (!product) {
+    return (
+      <div>
+        <span>There is no product context.</span>
+      </div>
+    )
+  }
+  if (loading) {
+    return (
+      <div>
+        <span>Loading...</span>
+      </div>
+    )
+  }
+  if (error) {
+    return (
+      <div>
+        <span>Error!</span>
+      </div>
+    )
+  }
+
+  return ( 
+    <div className={`${handles.countdown} c-muted-1 db tc`}>
+      <h1>{ `${timeRemaining.hours}:${timeRemaining.minutes}:${timeRemaining.seconds}` }</h1>
+    </div>
+  )
 }
 
 Countdown.schema = {
   title: 'editor.countdown.title',
   description: 'editor.countdown.description',
   type: 'object',
-  properties: {},
+  properties: {
+    targetDate: {
+      title: 'Final date',
+      description: 'Final date used in the countdown',
+      type: 'string',
+      default: null,
+    },
+  },
 }
 
 export default Countdown
